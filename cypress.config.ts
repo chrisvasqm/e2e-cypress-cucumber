bb47f783bc2c00f3b2cb5d704c4d116bba5c61f3
chrisvasqm <christianvasquezmelo@gmail.com>
2024-10-19 13:41:46 -0400

    Change default viewport

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
     specPattern: '**/*.feature',
     async setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
