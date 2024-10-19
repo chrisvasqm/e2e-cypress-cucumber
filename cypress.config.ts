ffa6c033ccf4d6e8e517eea69125c61e837c6a96
chrisvasqm <christianvasquezmelo@gmail.com>
2024-10-19 13:39:42 -0400

    Setup Cucumber

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    specPattern: '**/*.feature',
+    async setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on('file:preprocessor', createBundler({plugins: [createEsbuildPlugin(config)]}));
+      
+      return config;
+    }
+  }
+});
